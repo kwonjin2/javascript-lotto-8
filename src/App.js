00ca7bfe8fe6d8e2bc3728cd38c1f7f89c73a3c5
최권진 <rnjswls060@naver.com>
2025-11-02 15:42:45 +0900

    feat(App): 로또 구매할 금액 입력받기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseInput =
+      await Console.readLineAsync('구매 금액을 입력해 주세요.\n');
+  }
 }
 
 export default App;
