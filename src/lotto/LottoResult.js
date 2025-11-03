06675a672e6b98a3a2f3257785a5aaa508a2abdd
최권진 <rnjswls060@naver.com>
2025-11-03 19:10:00 +0900

    feat(LottoResult): LottoResult 클래스 기본 구조 정의

---
+++ src/lotto/LottoResult.js
@@ -0,0 +1,17 @@
+class LottoResult {
+  #winningRank = {
+    match3: 0,
+    match4: 0,
+    match5: 0,
+    match5AndBonus: 0,
+    match6: 0,
+  };
+
+  #prizeTable = {
+    1: 2000000000,
+    2: 30000000,
+    3: 1500000,
+    4: 50000,
+    5: 5000,
+  };
+}
