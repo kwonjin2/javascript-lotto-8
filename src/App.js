14f99d1520afff196e1c3f72bec2a7876f32b0a5
최권진 <rnjswls060@naver.com>
2025-11-03 15:00:27 +0900

    refactor(App): Lotto 클래스 사용하도록 리팩토링
    - App에서 로또 생성, 번호 정렬, 비교 로직 제거
    - Lotto.generateLottos 및 Lotto.compareWithWinningNumbers 활용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Console, Random } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
+import Lotto from './lotto/Lotto.js';
 
 class App {
   winningRank = {
@@ -22,9 +23,12 @@ class App {
       await Console.readLineAsync('구매 금액을 입력해 주세요.\n');
 
     const lottoCount = this.getLottoCount(purchaseInput);
-    const lottos = this.generateLottos(lottoCount);
+    const lottos = Lotto.generateLottos(lottoCount);
+
     Console.print(`${lottoCount}개를 구매했습니다.\n`);
-    lottos.forEach((lotto) => Console.print(`[${lotto.join(', ')}]`));
+    lottos.forEach((lotto) =>
+      Console.print(`[${lotto.getNumbers().join(', ')}]`)
+    );
 
     const winnerLottoNumbersInput = await Console.readLineAsync(
       '\n당첨 번호를 입력해 주세요.\n'
@@ -46,54 +50,24 @@ class App {
 
   getLottoCount(purchaseAmount) {
     const count = Number(purchaseAmount) / 1000;
-    this.winningRank.match3++;
     return count;
-  }
-
-  generateLottos(lottoCount) {
-    const result = [];
-
-    for (let i = 0; i < lottoCount; i++) {
-      result.push(
-        this.sortLottoNumbers(Random.pickUniqueNumbersInRange(1, 45, 6))
-      );
-    }
-
-    return result;
-  }
-
-  sortLottoNumbers(lotto) {
-    return lotto.sort((a, b) => a - b);
   }
 
   parseWinningNumbers(winningNumbers) {
     return winningNumbers.split(',').map((s) => Number(s));
   }
 
-  compareWithWinningNumbers(lotto, winningNumbers, bonusNumber) {
-    let matchCount = 0;
-    let hasBonus = false;
-
-    for (let i = 0; i < lotto.length; i++) {
-      if (winningNumbers.includes(lotto[i])) matchCount++;
-      if (lotto[i] === bonusNumber) hasBonus = true;
-    }
-
-    return { matchCount, hasBonus };
-  }
-
   updateWinningStatistics(lottos, winningNumbers, bonusNumber) {
     for (const lotto of lottos) {
-      const { matchCount, hasBonus } = this.compareWithWinningNumbers(
-        lotto,
+      const { matchCount, hasBonus } = lotto.compareWithWinningNumbers(
         winningNumbers,
         bonusNumber
       );
 
       if (matchCount === 3) this.winningRank.match3++;
       if (matchCount === 4) this.winningRank.match4++;
-      if (matchCount === 5 && hasBonus) this.winningRank.match5++;
-      if (matchCount === 5 && !hasBonus) this.winningRank.match5AndBonus++;
+      if (matchCount === 5 && !hasBonus) this.winningRank.match5++;
+      if (matchCount === 5 && hasBonus) this.winningRank.match5AndBonus++;
       if (matchCount === 6) this.winningRank.match6++;
     }
   }
