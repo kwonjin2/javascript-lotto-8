3d17645cdc1f87b62b404175daa00d46a93c57f6
최권진 <rnjswls060@naver.com>
2025-11-03 19:57:50 +0900

    refactor(App): InputHandler 클래스 연동 및 App 클래스 로직 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import Lotto from './lotto/Lotto.js';
 import LottoResult from './lotto/LottoResult.js';
+import InputHandler from './view/InputHandler.js';
 
 class App {
   async run() {
-    const purchaseInput =
-      await Console.readLineAsync('구매 금액을 입력해 주세요.\n');
-    this.validatePurchaseAmount(purchaseInput);
+    const purchaseInput = await InputHandler.readPurchaseAmount();
 
     const lottoCount = this.getLottoCount(purchaseInput);
     const lottos = Lotto.generateLottos(lottoCount);
@@ -15,20 +14,16 @@ class App {
       Console.print(`[${lotto.getNumbers().join(', ')}]`)
     );
 
-    const winnerLottoNumbersInput = await Console.readLineAsync(
-      '\n당첨 번호를 입력해 주세요.\n'
-    );
-    const winningNumbers = this.parseWinningNumbers(winnerLottoNumbersInput);
-    this.validateWinningNumbers(winningNumbers);
-
-    const bonusLottoNumberInput = await Console.readLineAsync(
-      '\n보너스 번호를 입력해 주세요.\n'
-    );
-    const bonusNumber = Number(bonusLottoNumberInput);
-    this.validateBonusNumber(bonusNumber, winningNumbers);
+    const winnerLottoNumbers = await InputHandler.readWinningNumbers();
+    const bonusLottoNumber =
+      await InputHandler.readBonusNumber(winnerLottoNumbers);
 
     const lottoResult = new LottoResult();
-    lottoResult.updateWinningStatistics(lottos, winningNumbers, bonusNumber);
+    lottoResult.updateWinningStatistics(
+      lottos,
+      winnerLottoNumbers,
+      bonusLottoNumber
+    );
     const totalPrize = lottoResult.getTotalPrize();
     const profitRate = lottoResult.calculateProfitRate(purchaseInput);
     const winningRank = lottoResult.getWinningRank();
@@ -39,10 +34,6 @@ class App {
   getLottoCount(purchaseAmount) {
     const count = Number(purchaseAmount) / 1000;
     return count;
-  }
-
-  parseWinningNumbers(winningNumbers) {
-    return winningNumbers.split(',').map((s) => Number(s));
   }
 
   printStatistics(totalPrize, profitRate, winningRank) {
@@ -57,46 +48,6 @@ class App {
     Console.print(`6개 일치 (2,000,000,000원) - ${winningRank.match6}개`);
     Console.print(`총 당첨금: ${totalPrize.toLocaleString()}원`);
     Console.print(`총 수익률은 ${profitRate}%입니다.`);
-  }
-
-  validatePurchaseAmount(purchaseAmount) {
-    const money = Number(purchaseAmount);
-    if (isNaN(money))
-      throw new Error('[ERROR] 구매 금액은 숫자로 입력해 주세요.');
-    if (money < 1000)
-      throw new Error('[ERROR] 구매 금액은 1,000원 이상이어야 합니다.');
-    if (money % 1000 !== 0)
-      throw new Error('[ERROR] 구매 금액은 1,000원 단위로 입력해 주세요.');
-  }
-
-  validateWinningNumbers(numbers) {
-    const parsedNumbers = numbers.map((n) => Number(n));
-
-    parsedNumbers.forEach((number) => {
-      if (isNaN(number))
-        throw new Error('[ERROR] 당첨 번호는 숫자로 입력해 주세요.');
-      if (number < 1 || number > 45)
-        throw new Error('[ERROR] 당첨 번호는 1 ~ 45 범위로 입력해 주세요.');
-    });
-
-    if (parsedNumbers.length !== 6)
-      throw new Error('[ERROR] 당첨 번호는 6개여야 합니다.');
-
-    const unique = new Set(parsedNumbers);
-    if (unique.size !== parsedNumbers.length)
-      throw new Error('[ERROR] 당첨 번호는 중복될 수 없습니다.');
-  }
-
-  validateBonusNumber(number, winningNumbers) {
-    const bonusNumber = Number(number);
-
-    if (isNaN(bonusNumber))
-      throw new Error('[ERROR] 보너스 번호는 숫자로 입력해 주세요.');
-    if (bonusNumber < 1 || bonusNumber > 45)
-      throw new Error('[ERROR] 보너스 번호는 1 ~ 45 범위로 입력해 주세요.');
-
-    if (winningNumbers.includes(bonusNumber))
-      throw new Error('[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.');
   }
 }
 
