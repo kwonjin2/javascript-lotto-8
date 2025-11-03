f4f3e8366051bb202f662f0b94cf4048d05a46a2
최권진 <rnjswls060@naver.com>
2025-11-03 12:44:47 +0900

    feat(App): 당첨 통계 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -102,6 +102,20 @@ class App {
     const profitRate = (totalPrize / purchaseAmount) * 100;
     return profitRate.toFixed(2);
   }
+
+  printStatistics(totalPrize, profitRate) {
+    Console.print('\n당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${this.winningRank.match3}개`);
+    Console.print(`4개 일치 (50,000원) - ${this.winningRank.match4}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${this.winningRank.match5}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${this.winningRank.match5AndBonus}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${this.winningRank.match6}개`);
+    Console.print(`총 당첨금: ${totalPrize.toLocaleString()}원`);
+    Console.print(`총 수익률은 ${profitRate}%입니다.`);
+  }
 }
 
 export default App;
