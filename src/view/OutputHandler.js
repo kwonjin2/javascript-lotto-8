eb9f0c65ed85651b274f0a2e6b23e913ffc9735c
최권진 <rnjswls060@naver.com>
2025-11-03 20:23:50 +0900

    feat(OutputHandler): App에서 처리하던 출력 관련 기능을 OutputHandler 클래스로 이동

---
+++ src/view/OutputHandler.js
@@ -0,0 +1,29 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class OutputHandler {
+  static printLottoCount(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.\n`);
+  }
+
+  static printLotto(lottos) {
+    lottos.forEach((lotto) =>
+      Console.print(`[${lotto.getNumbers().join(', ')}]`)
+    );
+  }
+
+  static printStatistics(totalPrize, profitRate, winningRank) {
+    Console.print('\n당첨 통계');
+    Console.print('---');
+    Console.print(`3개 일치 (5,000원) - ${winningRank.match3}개`);
+    Console.print(`4개 일치 (50,000원) - ${winningRank.match4}개`);
+    Console.print(`5개 일치 (1,500,000원) - ${winningRank.match5}개`);
+    Console.print(
+      `5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningRank.match5AndBonus}개`
+    );
+    Console.print(`6개 일치 (2,000,000,000원) - ${winningRank.match6}개`);
+    Console.print(`총 당첨금: ${totalPrize.toLocaleString()}원`);
+    Console.print(`총 수익률은 ${profitRate}%입니다.`);
+  }
+}
+
+export default OutputHandler;
